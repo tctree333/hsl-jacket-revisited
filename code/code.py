085ea6a6a2475d4a254105a19d3fc6fe099f4fc9
Tomi Chen <32943907+tctree333@users.noreply.github.com>
2023-08-11 09:57:04 -0700

    add rainbow pattern

--- code/code.py
+++ code/code.py
@@ -3,6 +3,7 @@ import digitalio
 import analogio
 import neopixel
 import colorsys
+import time
 
 hue_button = digitalio.DigitalInOut(board.GP16)
 hue_button.direction = digitalio.Direction.INPUT
@@ -16,8 +17,10 @@ light_button.pull = digitalio.Pull.DOWN
 
 pot = analogio.AnalogIn(board.GP26)
 
-pixels = neopixel.NeoPixel(board.GP15, 15, brightness=0.1)
+pixels = neopixel.NeoPixel(board.GP15, 30, brightness=0.1)
 pixels.fill((0, 255, 255))
+
+num_pixels = 30
 
 hue = .5
 sat = 1
@@ -31,23 +34,66 @@ def avg_pot():
     for _ in range(15000):
         total += pot.value
     return 1 - min(1, max(0, ((total / 15000) - min_pot) / (max_pot - min_pot)))
+    
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b)
+
+
+def rainbow_cycle(j):
+    for i in range(num_pixels):
+        pixel_index = (i * 256 // num_pixels) + j
+        pixels[i] = wheel(pixel_index & 255)
+    pixels.show()
 
 led_change = False
+rainbow = True
+last_press = 0
+j = 0
 while True:
-    current_pot = avg_pot()
+    t = time.monotonic()
+    if last_press != 0 and hue_button.value and (t-last_press) < .5:
+        last_press = 0
+        rainbow = not rainbow
+    elif hue_button.value:
+        last_press = t
 
-    if hue_button.value:
-        hue = avg_pot()
-        led_change = True
-    if sat_button.value:
-        sat = avg_pot()
-        led_change = True
-    if light_button.value:
-        light = avg_pot()
-        led_change = True
-
-    if led_change:
-        pixels.fill(tuple(map(lambda x: int(x), ((255, 255, 255) if sat == 0 else colorsys.hsv_to_rgb(hue, sat, 1)))))
-        pixels.brightness = light
-        pixels.show()
-        led_change = False
+    if rainbow:
+        rainbow_cycle(j)
+        j += 1
+        j = j % 255
+    else:
+        current_pot = avg_pot()
+    
+        if hue_button.value:
+            hue = avg_pot()
+            led_change = True
+        if sat_button.value:
+            sat = avg_pot()
+            led_change = True
+        if light_button.value:
+            light = avg_pot()
+            led_change = True
+    
+        if led_change:
+            pixels.fill(tuple(map(lambda x: int(x), ((255, 255, 255) if sat == 0 else colorsys.hsv_to_rgb(hue, sat, 1)))))
+            pixels.brightness = light
+            pixels.show()
+            led_change = False
